ee1110cba9ffa35c4cd03adb151127253e1db470
WodaMineralna <raknieborak65@gmail.com>
2025-06-29 16:17:30 +0200

    Adding <li> opening / closing functionality
    - Using the Context API, so only one of the existing <li> items can be [...]
      -> open at once

--- src/components/Accordion/Accordion.jsx
+++ src/components/Accordion/Accordion.jsx
@@ -1,3 +1,43 @@
+import { createContext, useContext, useState } from "react";
+
+const AccordionContext = createContext({
+  openItemId: null,
+  openItem: () => {},
+  closeItem: () => {},
+});
+
+export function useAccordionContext() {
+  const ctx = useContext(AccordionContext);
+
+  if (!ctx) {
+    throw new Error(
+      "Accordtion-related components must be wrapped by <Accordion>."
+    );
+  }
+
+  return ctx;
+}
+
 export default function Accordion({ children, className }) {
-  return <ul className={className}>{children}</ul>;
+  const [openItemId, setOpenItemId] = useState(null);
+
+  function openItem(id) {
+    setOpenItemId(id);
+  }
+
+  function closeItem() {
+    setOpenItemId(null);
+  }
+
+  const contextValue = {
+    openItemId,
+    openItem,
+    closeItem,
+  };
+
+  return (
+    <AccordionContext.Provider value={contextValue}>
+      <ul className={className}>{children}</ul>
+    </AccordionContext.Provider>
+  );
 }
--- src/components/Accordion/AccordionItem.jsx
+++ src/components/Accordion/AccordionItem.jsx
@@ -1,8 +1,24 @@
-export default function AccordionItem({ title, className, children }) {
+import { useAccordionContext } from "./Accordion";
+
+export default function AccordionItem({ id, title, className, children }) {
+  const { openItemId, openItem, closeItem } = useAccordionContext();
+
+  const isOpen = openItemId === id;
+
+  function handleClick() {
+    if (isOpen) {
+      closeItem(id);
+    } else {
+      openItem(id);
+    }
+  }
+
   return (
     <li className={className}>
-      <h3>{title}</h3>
-      <div>{children}</div>
+      <h3 onClick={handleClick}>{title}</h3>
+      <div className={`accordion-item-content ${isOpen ? "open" : ""}`}>
+        {children}
+      </div>
     </li>
   );
 }
