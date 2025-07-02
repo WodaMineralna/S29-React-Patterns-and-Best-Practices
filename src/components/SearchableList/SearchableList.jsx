5da71812af4c1acdc9f54ec9b19081b976b53494
WodaMineralna <raknieborak65@gmail.com>
2025-07-02 17:48:22 +0200

    Added Debouncing for the Search functionality
    -  So the Searched data updates after 500ms of no additional Search Term modifications

--- src/components/SearchableList/SearchableList.jsx
+++ src/components/SearchableList/SearchableList.jsx
@@ -1,14 +1,25 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
 
 export default function SearchableList({ items, itemKeyFn, children }) {
   const [searchTerm, setSearchTerm] = useState("");
+  const lastChange = useRef();
 
   const searchResults = items.filter((item) =>
     JSON.stringify(item).toLowerCase().includes(searchTerm.toLowerCase())
   );
 
   function handleChange(event) {
-    setSearchTerm(event.target.value);
+    // if we have a currently running timer - we clear it, and start a new one
+    if (lastChange.current) {
+      clearTimeout(lastChange.current);
+    }
+
+    // starting a new timeout, which will update our State in 500ms
+    lastChange.current = setTimeout(() => {
+      // ^ manually clearing the Timer ID from the Ref if the timer has expired, so it's not equal to 'true' in the if-statement above 
+      lastChange.current = null;
+      setSearchTerm(event.target.value);
+    }, 500);
   }
 
   return (
